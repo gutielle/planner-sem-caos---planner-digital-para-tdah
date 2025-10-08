5cce7086ff92b4fa0ee466712eabef3895ac71b1
gutielle <zgutielle@gmail.com>
2025-10-08 15:45:55 -0300

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Members from './Members';
 import './index.css';
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 
 // Initialize Vercel Speed Insights
 injectSpeedInsights();
@@ -16,6 +18,11 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/members" element={<Members />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
